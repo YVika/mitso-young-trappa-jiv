f9e6ea8e4edcb0a336a4775ad5e1d1636e291db6
Victor Korolenok <korolionokviktor@gmail.com>
2023-03-13 09:08:53 +0300

    feat: rename complete

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const dir = "files";
+const newName = "properFilename.md";
+const oldName = "wrongFilename.txt";
+
 const rename = async () => {
-    // Write your code here 
+    let arr = await fs.readdir(path.join(__dirname, dir));
+    if(arr.includes(!oldName))
+    {
+        console.log("FS operation failed");
+    }
+    else if(arr.includes(newName))
+    {
+        console.log("FS operation failed");
+    }
+    else
+    {
+        await fs.rename(path.join(__dirname, dir, oldName), path.join(__dirname, dir, newName));
+    }
 };
 
 await rename();
