3f9008ea078c5c440bec9e7a6f44c395c0f9c5bc
Victor Korolenok <korolionokviktor@gmail.com>
2023-03-13 09:08:12 +0300

    feat: list complete

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs/promises";
+import path from "path";
+import * as url from "url";
+
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const dir = "files";
+
 const list = async () => {
-    // Write your code here 
+    let arr = await fs.readdir(__dirname);
+    if(!arr.includes(dir))
+    {
+        console.log("FS operation failed");
+    }
+    else 
+    {
+        arr = await fs.readdir(path.join(__dirname, dir));
+        for (let i = 0; i < arr.length; i++) {
+            console.log(arr.at(i));
+        }
+    }
 };
 
 await list();
